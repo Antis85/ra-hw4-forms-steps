907bc1910df01ad8fa596b154f0333d06c30d7c2
Anton Bugryakov <antis_amdg@mail.ru>
2021-10-21 22:03:55 +0300

    commit-1-hw-done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/ag0ohadhw1ls2fva/branch/main?svg=true)](https://ci.appveyor.com/project/Antis85/ra-hw4-forms-steps/branch/main)
+
 # ra-hw4-forms-steps
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Tracker from './components/Tracker';
+
+export default function App() {
+  const table = [];
+  const form = {
+    id: '',
+    date: '',
+    distance: '',
+  };
+  return (
+    <div className="App">
+      <Tracker className="Tracker" initTable={table} initForm={form} />
+    </div>
+  );
+}
